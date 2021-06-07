b27515a012214ecd935f9c5490086c3b72ce66d7
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-07 18:46:02 -0500

    change to ran after

--- short-story.rb
+++ short-story.rb
@@ -1,3 +1,3 @@
 Stanley went for a walk in the park and saw three ducks.
-He chased them but did not catch any of them.
+He ran after them but did not catch any of them.
 Then he went to get ice cream because he was hungry.
