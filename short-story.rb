0c6946f90ff3cad54f5dc89ded9066e0c8caf314
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-07 18:45:25 -0500

    replace home with icecream

--- short-story.rb
+++ short-story.rb
@@ -1,3 +1,3 @@
 Stanley went for a walk in the park and saw three ducks.
 He chased them but did not catch any of them.
-Then he went home because he was tired.
+Then he went to get ice cream because he was hungry.
